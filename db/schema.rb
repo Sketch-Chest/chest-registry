530ddf71733cf135387e4b3d64173c0633db06ec
Yasuaki Uechi <uetchy@randompaper.co>
2015-03-17 14:25:55 +0900

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150314032402) do
+ActiveRecord::Schema.define(version: 20150314032404) do
 
   create_table "packages", force: :cascade do |t|
     t.string   "name",                        null: false
